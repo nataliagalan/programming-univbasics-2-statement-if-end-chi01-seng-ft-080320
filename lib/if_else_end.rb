6706d13c0a1fee028f46ce459210244cb50c635e
Natalia Galan <n8galan@gmail.com>
2020-06-26 20:56:54 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,4 +1,11 @@
 # Write your solution here
+# If the current second is an even number, output Even!
+# If the current second is an odd number, output Odd!
+current_time = Time.now
+current_time = current_time.to_i
 
-this_year = Time.now.year
-puts "Hey, it's #{this_year}"
+if current_time % 2 == 0
+  puts "Even!"
+else
+  puts "Odd!"
+end
